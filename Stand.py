eed7affc9e38854bb3389d74ed6c2c7221b63e8a
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-12-08 00:02:07 -0400

    feat(Stand class): refactor constructor and add some methods to manipulate pods list
    This commit refactor the constructor by setting the values of the instance variables
    from the moment the classes are instanciated instead of doing it manually after instanciation.
    
    It also adds thre new methods:
    	create_pod(self)
    	get_pods(self)
    	show_pods(self)

--- Stand.py
+++ Stand.py
@@ -1,13 +1,37 @@
 from abc import ABC
+from Pods import Pod
+from helper_module import print_newline
 
 class Stand(ABC):
 
-    def __init__(self):
-        self.name = None
-        self.row = None
-        self.column = None
-        self.num_pods = None
-        self.stand = list()
+    def __init__(self, name, row, column, num_pods):
+        self.name = name
+        self.row = row # All the rows that it has with counting different pods
+        self.column = column # All the columns
+        self.num_pods = num_pods # Quantity of pods wanted for the stand.
+        self.pod_rows = int(self.row / self.num_pods)
+        self.pod_columns = column
+        self.pods = list() # A list of all pods inside the stand
+
+    def create_pod(self):
+        """This method will create and store pods inside the stand list."""
+        
+        pod = Pod(self.pod_rows, self.pod_columns)
+        self.add_pod_to_stand(pod)
+        
+        return pod
+
+    def get_pods(self):
+        """This method will return a list of the pods for this stand."""
+        
+        return self.pods
+
+    def show_pods(self):
+        """This method will show all pods in the pods list."""
+
+        for i in self.pods:
+            i.show_pod()  
+            print_newline()  
 
     def get_name(self):
         """This method will return the name."""
@@ -41,31 +65,27 @@ class Stand(ABC):
         """This method will let you change the number of pods in this specific stand."""
         self.num_pods = num_pods
 
-    def get_stand(self):
-        """This method will return the stand list that contain all the pods objects."""
-        return self.stand
-
     def add_pod_to_stand(self, pod):
         """This method will add a pod to the stand list that contain all the pods objects."""
-        self.stand.append(pod)
+        self.pods.append(pod)
 
     def remove_pod_from_stand(self, pod_id):
         """This method will remove pod from stand list that contain all the pods objects. The parameter should be the pods id."""
         found_pod = False
 
-        # Loop through each pod in our stand list.
-        for i in self.stand:
+        # Loop through each pod in our pods list.
+        for i in self.pods:
 
             # If we find them, remove them from the list.
             if i.id == pod_id:
-                self.stand.remove(i)
+                self.pods.remove(i)
                 # set our indicator to True
-                found_hero = True
+                found_pod = True
        
         # If we looped through our list and did not find our pod, the indicator would never get change, so return 'Pod not found!'
         if not found_pod:
             print(f'Pod not found!')
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  
     pass
